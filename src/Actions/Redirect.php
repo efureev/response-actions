b1448124ccefec3aec327b427ac55b0b12862bae
Fureev Eugene <fureev.cyprus@gmail.com>
2023-11-30 15:21:05 +0200

    feat: add `pushToRoute` into Redirect

--- src/Actions/Redirect.php
+++ src/Actions/Redirect.php
@@ -14,10 +14,14 @@ class Redirect extends AbstractAction
     public const TARGET_TOP    = '_top';
     public const TARGET_PARENT = '_parent';
 
+    public const TYPE_NATIVE = 'native';
+    public const TYPE_ROUTER = 'router';
+
     public function __construct(
         protected string $url,
         protected string $target = self::TARGET_BLANK,
-        protected int $code = 302
+        protected int $code = 302,
+        protected string $type = self::TYPE_NATIVE
     ) {
     }
 
@@ -29,7 +33,20 @@ class Redirect extends AbstractAction
         return [
             'url'    => $this->url,
             'target' => $this->target,
+            'type'   => $this->type,
             'code'   => $this->code,
         ];
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public static function pushToRoute(string $url): self
+    {
+        return new self($url, type: self::TYPE_ROUTER);
     }
 }
